22ea0205f14ec6fe5e282b9c461fce47b5c75d97
641210592@qq.com <syj641210592>
2020-09-16 22:38:51 +0800

    master

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 from selenium.webdriver import Edge
-from com_func.confread import config
 from com_func.basepath import testpath
+from selenium.webdriver.common.by import By
 from selenium import webdriver
-import time
 
 # 浏览器(指定驱动路径)
 brown = Edge(executable_path=testpath.WebDrive)
-brown.implicitly_wait(20)
-brown.get(config.get("URL", "url"))
-element_kc = brown.find_element_by_xpath(r'//div[@id="js-searchbox"]//span[text()="课程"]')
-element_kc.click()
-element_search_input = brown.find_element_by_xpath(r'//input[@id="js_keyword"]')
-element_search_input.send_keys("孙忘")
-element_search_home = brown.find_element_by_xpath(r'//ul[@id="js-tab"]//h2[text()="主页"]')
-element_kc2 = brown.find_element_by_xpath(r'//ul[@id="js-tab"]//h2[contains(.,"课程")]')
+# 设置隐性等待
+brown.implicitly_wait(15)
+# 访问网站
+brown.get(r'https://www.ketangpai.com/User/login.html')
+try:
+    element_user = brown.find_element(By.XPATH, r'//div[@id="login"]//input[@type="text" and @name="account"]')
+    element_user.send_keys("6412xxxxx@qq.com")
+    element_pwd = brown.find_element(By.XPATH, r'//div[@id="login"]//input[@type="password" and @name="pass"]')
+    element_pwd.send_keys("33xxxxxsun")
+    element_login = brown.find_element(By.XPATH, r'//div[@class="padding-cont pt-login" ]/a[@href="javascript:;" and text()="登录"]')
+    element_login.click()
+finally:
+    brown.quit()
 
-'//div[@id="js-searchbox"]//span[text()="课程"]'
-'//input[@id="js_keyword"]''
-'//ul[@id="js-tab"]//h2[contains(.,"主页")]'
-'//ul[@id="js-tab"]//h2[contains(.,"课程")]'
-'//ul[@id="js-tab"]//h2[contains(.,"老师")]'
-'//ul[@id="js-tab"]//h2[contains(.,"关于我们")]'
-'//div[@id="js-agency-nav"]/following-sibling::div//h3[contains(.,"软件测试之python")]'
-'//div[@id="js-course-block"]//h4[contains(.,"软件测试自学")]/following-sibling::div//span[text()="免费"]'
-'//div[@id="js-course-block"]//h4[contains(.,"软件测试自学")]/following-sibling::div//span[text()="免费"]'
-'//div[@id="js-course-block"]//h4[contains(.,"软件测试自学")]/following-sibling::div//span[contains(.,"1663")]'
+
+from selenium.webdriver import Edge
+from com_func.basepath import testpath
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = Edge(executable_path=testpath.WebDrive)
+driver.get(r'https://kf.qq.com/product/weixin.html')
+try:
+    element_plus = driver.find_element(By.XPATH, r'//i[@id="m_1453"]')
+    element_plus.click()
+    element_wx = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, r'//i[@id="m_1453"]/ancestor::a[text()="手机版微信"]//following-sibling::dl//a[text()="微信群"]'))
+    )
+    element_wx.click()
+    element_wx_set = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, r'//div[@id="faqlist"]//a[text()="微信群创建及设置方法"]'))
+    )
+    element_wx_set.click()
+finally:
+    driver.quit()
