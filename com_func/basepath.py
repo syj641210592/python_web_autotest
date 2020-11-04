c4a7a097521df7470226dadaebd8ed51c40af751
641210592@qq.com <syj641210592>
2020-11-04 23:41:57 +0800

    master

--- com_func/basepath.py
+++ com_func/basepath.py
@@ -1,3 +1,10 @@
+'''
+Description: 
+Author: sunwang
+Date: 2020-09-14 21:14:13
+LastEditTime: 2020-11-04 23:34:00
+LastEditors: sunwang
+'''
 from com_func.confread import config
 import os
 
@@ -5,7 +12,7 @@ class TestPath(object):
 
     def __init__(self) :
         self.BasePath = os.getcwd()
-        self.ScreenShot = os.path.join(os.sep, self.BasePath, "screenshot")
+        self.ScreenShot = os.path.join(os.sep, self.BasePath, config.get("ENV", "screenshot_path"))
         self.ReportPath = os.path.join(os.sep, self.BasePath, config.get("ENV", "report_path"))
         self.LogDirPath = os.path.join(os.sep, self.BasePath, config.get("ENV", "log_path"))
         self.TestData = os.path.join(os.sep, self.BasePath, "test_case_data", "case_data.yaml")
--- html_page/base_page.py
+++ html_page/base_page.py
@@ -2,15 +2,17 @@
 Description: 基础页面功能
 Author: sunwang
 Date: 2020-09-29 22:28:30
-LastEditTime: 2020-11-03 23:50:02
+LastEditTime: 2020-11-04 23:41:53
 LastEditors: sunwang
 '''
+import os
+import time
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from com_func.log import logger
-
+from com_func.basepath import testpath
 
 class BasePage(object):
     def __init__(self, driver: WebDriver):
@@ -31,12 +33,20 @@ class BasePage(object):
     def find_ele(self, method="XPATH", pat="", pat_params="", type="located", wait_time=10):
         '''查找页面元素'''
         try:
+            self.params = pat
+
             if pat_params != "":
-                 pat = pat % pat_params
+                pat = pat % pat_params
+                self.params = pat
+
             if type == "located":
                 self.ele = WebDriverWait(self.driver, wait_time).until(EC.presence_of_element_located((getattr(By, method), pat)))
+            elif type == "clickable":
+                self.ele = WebDriverWait(self.driver, wait_time).until(EC.element_to_be_clickable((getattr(By, method), pat)))
+            elif type == "selected":
+                self.ele = WebDriverWait(self.driver, wait_time).until(EC.element_located_to_be_selected((getattr(By, method), pat)))
         except Exception as e:
-            logger.error(f"查找页面元素{pat_params}失败", exc_info=True)
+            logger.error(f"查找页面元素{self.params}失败", exc_info=True)
             self.ele = ""
             raise e
         finally:
@@ -45,17 +55,53 @@ class BasePage(object):
     def send_keys(self, send_text):
         '''输入文本'''
         self.ele.clear()
-        self.ele.send_keys(send_text)
+        self.ele.send_keys(str(send_text))
 
     def get_ele_text(self):
         '''获取元素文本'''
         return self.ele.text
 
-    def get_ele_attribute(self, class_name, ele=""):
+    def ele_click(self):
+        '''点击元素'''
+        try:
+            self.ele.click()
+        except Exception as e:
+            logger.error(f"点击页面元素{self.params}失败", exc_info=True)
+            self.ele = ""
+            raise e
+
+    def get_ele_attribute(self, attr, ele=""):
         '''获取元素属性'''
         if ele == "":
             ele = self.ele
-        return ele.get_attribute(class_name)
+        return ele.get_attribute(attr)
 
+    def switch_to_windows_frame(self, frame="default"):
+        '''切换页面frame'''
+        if frame == "default":
+            self.driver.switch_to.default_content()
+        elif frame == "parent":
+            self.driver.switch_to.parent_frame()
+        else:
+            self.driver.switch_to.frame(frame)
+
+    def windows_handle(self, handle="handles"):
+        '''切换窗口句柄'''
+        if handle == "handle":
+            return self.driver.current_window_handle()
+        elif handle == "handles":    
+            return self.driver.window_handles()
+        else:
+            self.driver.switch_to_window(handle)
+
+    def save_screenshot(self, filename, path=""):
+        '''页面截图'''
+        # 时间戳
+        date_desc = time.strftime(r"%Y-%m_%d-%h_%H_%M_%S")
+        # 定义截图文件名
+        filename = os.path.join(os.sep, testpath.ScreenShot, date_desc+filename+".png")
+        # 截图
+        self.driver.save_screenshot(filename)
+        
     def quit_driver(self):
         self.driver.quit()
