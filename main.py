f2bfc5cd74d45462b36ce0868a03636f87e14d1d
641210592@qq.com <syj641210592>
2020-12-16 21:59:47 +0800

    master

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Description: web自动化
 Author: sunwang
 Date: 2020-09-14 21:04:05
-LastEditTime: 2020-12-15 22:53:33
+LastEditTime: 2020-12-16 21:59:39
 LastEditors: sunwang
 '''
 
@@ -10,4 +10,4 @@ LastEditors: sunwang
 import pytest
 import os
 pytest.main(['-s', '-v', "--reruns", "3", "--reruns-delay", "5" , "--alluredir=result/report"])
-os.system('--alluredir=result/report')
+os.system('allure serve result/report')
